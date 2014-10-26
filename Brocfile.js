d9a36cba94dd086559a7f8dc3b9477692e39654e
Tom Cocca <tom.cocca@gmail.com>
2014-10-26 11:08:03 -0400

    use Bootstrap theme and JS

--- Brocfile.js
+++ Brocfile.js
@@ -2,7 +2,12 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp();
+var app = new EmberApp({
+  'ember-cli-bootstrap': {
+      'importBootstrapJS': true,
+      'importBootstrapTheme': true
+    }
+});
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
